efd65c6b363886d97d3169903ff00f3b78e6204a
Ashish Rao <ashishrao2598@gmail.com>
2020-07-01 12:38:27 +0530

    update homepage title

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.red,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Release Demo'),
+      home: MyHomePage(title: 'Flutter Release 0.1.6'),
     );
   }
 }
